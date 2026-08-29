df5401f24b52cdf2d96789662a0996baa78e308b
Srushti Patil <srushtipatil.cloud@gmail.com>
2026-08-29 21:49:08 +0530

    Add Terraform provider, version, and variable configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+﻿variable "subscription_id" {
+  description = "Azure subscription ID where resources will be managed"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "centralindia"
+}
+
+variable "security_resource_group_name" {
+  description = "Name of the Security/Admin resource group"
+  type        = string
+  default     = "rg-cross-account-iam-security"
+}
+
+variable "workload_resource_group_name" {
+  description = "Name of the Workload resource group"
+  type        = string
+  default     = "rg-workload-environment"
+}
